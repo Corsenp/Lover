3e18c34343f0c0d828a3f03b4bbe5919197b4530
Pierre Corsenac <pierre.corsenac@gmail.com>
2024-04-24 10:16:38 +0200

    Initial Commit

---
+++ Lover/LoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoverApp.swift
+//  Lover
+//
+//  Created by Corsenac Pierre on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct LoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
